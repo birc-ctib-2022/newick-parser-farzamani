6aa057720d6d70a95d09eebdee661e02e737b0c3
Farhad Zamani <zmnfrhd@gmail.com>
2022-12-01 18:53:35 +0100

    testing

--- src/newick.py
+++ src/newick.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import Union, cast
+from typing import Union, cast, Generic, Optional, TypeVar
 import re
 
 
@@ -16,6 +16,64 @@ def tokenize(tree: str) -> list[str]:
     ['(', 'A', '(', 'B', 'C', ')', ')']
     """
     return re.findall(r'[()]|\w+', tree)
+
+T = TypeVar('T')
+
+@dataclass
+class Link(Generic[T]):
+    """A link in a linked list."""
+
+    head: T
+    tail: List[T]
+
+    def __init__(self, head):
+        self.head = head
+        self.next = None
+
+
+List = Optional[Link[T]]
+
+
+class Stack(Generic[T]):
+    """A stack of elements of (generic) type T."""
+
+    def __init__(self) -> None:
+        """Create a new stack of values of type T."""
+        # FIXME: code here
+        self.stack = None
+
+    def push(self, x: T) -> None:
+        """Push x on the top of this stack."""
+        # FIXME: code here
+        if self.stack == None:
+            self.stack = Link(x)
+        else:
+            new_node = Link(x)
+            new_node.next = self.stack
+            self.stack = new_node
+
+    def top(self) -> T:
+        """Return the top of the stack."""
+        # FIXME: code here
+        if self.is_empty():
+            return None
+        else:
+            return self.stack.head
+
+    def pop(self) -> T:
+        """Pop the top element off the stack and return it."""
+        # FIXME: code here
+        if self.is_empty():
+            return None
+        else:
+            pop_node = self.stack.head
+            self.stack = self.stack.next
+            return pop_node
+
+    def is_empty(self) -> bool:
+        """Test if the stack is empty."""
+        # FIXME: code here
+        return True if self.stack == None else False
 
 
 @dataclass(repr=False)
@@ -42,6 +100,7 @@ class Node:
 
     def __str__(self) -> str:
         """Simplified text representation."""
+        pass
         return f"({','.join(str(child) for child in self.children)})"
     __repr__ = __str__
 
@@ -57,4 +116,33 @@ def parse(tree: str) -> Tree:
     >>> parse("(A, (B, C))")
     (A,(B,C))
     """
-    ...
+    stack = Stack()
+
+    tree = tokenize(tree)
+    for ele in tree:
+        match ele:
+            case ')':
+                node = Stack()
+                node.push(ele)
+                while stack.top() != '(':
+                    node.push(stack.pop())
+                node.push(stack.pop())
+
+                children = Stack()
+                while node.is_empty() == False:
+                    children.push(node.pop())
+                
+                stack.push(Node(node))
+            case '(':
+                stack.push(ele)
+            case _:
+                stack.push(Leaf(ele))
+    
+    return cast(Tree, stack.pop())
+
+def main():
+    print(parse("(A, (B, C))"))
+    
+
+if __name__ == "__main__":
+    main()
